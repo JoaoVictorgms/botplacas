6bfb56f848af1a84223d9522a1fa2379d659d9cf
joao victor gomes <joao-iccz@hotmail.com>
2024-05-31 19:55:14 -0300

    Removendo

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,8 @@
 import asyncio
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dotenv import load_dotenv
 import os
 import pandas as pd
-import threading
-from telegram import Update
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -19,34 +17,26 @@ placas_df = pd.read_excel('jardiel_base.xlsx', usecols='R')
 placas = placas_df.iloc[:, 0].dropna().tolist()
 df_all_info = pd.read_excel('jardiel_base.xlsx')
 
-def check_placa_info(placa, resultados):
+def check_placa_info(placa):
+    resultados = []
     for index, row in df_all_info.iterrows():
         if str(row.iloc[17]).strip().upper() == placa:
             resultados.append(row.to_dict())
-            return
+            return resultados
     resultados.append(None)
+    return resultados
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update, context):
     await update.message.reply_text('Envie uma placa de carro para verificar se ela está na planilha.')
 
-async def verificar_placa(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def verificar_placa(update, context):
     placa = update.message.text.strip().upper()
 
     if not placa:
         await update.message.reply_text('Por favor, envie uma placa válida.')
         return
 
-    num_threads = 10
-    resultados = []
-    threads = []
-
-    for _ in range(num_threads):
-        thread = threading.Thread(target=check_placa_info, args=(placa, resultados))
-        threads.append(thread)
-        thread.start()
-
-    for thread in threads:
-        thread.join()
+    resultados = await asyncio.to_thread(check_placa_info, placa)
 
     if any(resultados):
         result = next((res for res in resultados if res is not None), None)
@@ -60,14 +50,15 @@ async def verificar_placa(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     else:
         await update.message.reply_text(f'A placa {placa} não está na planilha.')
 
-async def main() -> None:
-    try:
-        application = Application.builder().token(BOT_TOKEN).build()
-        application.add_handler(CommandHandler("start", start))
-        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, verificar_placa))
-        await application.run_polling()
-    except RuntimeError as exc:
-        print(f"Erro ao executar o bot: {exc}")
+async def main():
+    updater = Updater(token=BOT_TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, verificar_placa))
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     asyncio.run(main())
